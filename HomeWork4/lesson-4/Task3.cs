512751829a713901324a9ba942d4a870b2ce6d7b
oleg_yakovlev <fireball2010zp@gmail.com>
2022-02-07 16:14:56 +0200

    решение HomeWork4, в котором 4 класса (4 задания соответственно)

---
+++ HomeWork4/lesson-4/Task3.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  Яковлев Олег. Урок 4.
+    3.
+    а) Дописать класс для работы с одномерным массивом. 
+    Реализовать конструктор, создающий массив определенного размера и заполняющий массив числами от начального значения с заданным шагом. 
+    Создать свойство Sum, которое возвращает сумму элементов массива, 
+    метод Inverse, возвращающий новый массив с измененными знаками у всех элементов массива (старый массив, остается без изменений), 
+    метод Multi, умножающий каждый элемент массива на определённое число, 
+    свойство MaxCount, возвращающее количество максимальных элементов.
+*/
+
+namespace lesson_4
+{
+    public class OneDimArray
+    {
+        #region Поля
+
+        private int[] arr;
+
+        #endregion
+
+        #region Свойства
+
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойство возвращает сумму элементов массива
+        /// </summary>
+        public int Sum
+        {
+            get 
+            { 
+                int s = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    s = s + arr[i];
+                }
+                return s; 
+            }
+        }
+
+        /// <summary>
+        /// Свойство возвращает количество максимальных элементов массива
+        /// </summary>
+        public int MaxCount
+        {
+            get
+            {
+                int max = arr[0];
+                int count = 1;
+
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                        count = 1;
+                    }
+                    else if (arr[i] == max)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        #endregion
+
+        #region Конструкторы
+
+        public OneDimArray(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        /// <summary>
+        /// Конструктор, создающий массив определенного размера и заполняющий массив числами от начального значения с заданным шагом.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="Amount">Количество элементов массива</param>
+        /// <param name="a">Начальное значение</param>
+        /// <param name="step">Шаг</param>
+        public OneDimArray(int Amount, int a, int step )
+        {
+
+            arr = new int[Amount]; // инициализация кол-ва элементов массива
+
+            for (int i = 0; i < Amount; i++)
+            {
+                arr[i] = a + step * i;
+
+                Console.Write($"{arr[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+
+
+        #region Публичные методы 
+
+        public int GetElement(int index)
+        {
+            return arr[index];
+        }
+
+        public void SetElement(int index, int value)
+        {
+            arr[index] = value;
+        }
+
+        /// <summary>
+        /// Метод вывода массива
+        /// </summary>
+        public void PrintArray()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}\t");
+            }
+                Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Метод меняющий знаки элементов массива (передаёт значения в новый массив)
+        /// </summary>
+        public void Inverse()
+        {
+            int[] arr1 = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr1[i] = arr[i] * -1;
+                Console.Write($"{arr1[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Метод, умножающий каждый элемент массива на определённое число
+        /// </summary>
+        /// <param name="n"></param>
+        public void Multi(int n)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i] * n;
+                Console.Write($"{arr[i]}\t");
+            }
+            Console.WriteLine();
+        }
+
+        #endregion
+
+    }
+
+    internal class Task3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во элементов массива: ");
+            int Amount = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение первого элемента массива: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение шага, с которым будет заполнен массив: ");
+            int step = int.Parse(Console.ReadLine());
+
+            OneDimArray array01 = new OneDimArray(Amount, a, step);
+
+            Console.WriteLine("Сумма элементов массива = " + array01.Sum);
+
+            Console.WriteLine("\nМассив с измененными знаками у всех элементов массива (старый массив без изменений): ");
+            array01.Inverse();
+
+            Console.Write("\nДля демонстрации метода Multi (умножающего каждый элемент массива на определённое число) введите число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            array01.Multi(n);
+
+            Console.WriteLine("Количество максимальных элементов в массиве = " + array01.MaxCount);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork4/lesson-4/Task4.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+    /* Яковлев Олег. Урок 4.
+        4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. 
+        Создайте структуру Account, содержащую Login и Password.
+    */
+
+namespace lesson_4
+{
+    /// <summary>
+    /// Логин и пароль
+    /// </summary>
+    struct Account
+    {
+        private string[] arr;
+        
+        public Account(string fileName)
+        {
+            string[] arr = new string[2];
+            this.arr = arr;
+            this.arr = LoadArrayFromFile(fileName);
+        }
+
+        private string[] LoadArrayFromFile(string fileName) // метод загрузки из файла
+        {
+            if (!File.Exists(fileName)) // проверка наличия файла
+                return null;
+
+            string[] arr = new string[2];
+            int counter = 0;
+
+            StreamReader sr = new StreamReader(fileName); // вспомогательный класс, устанавливает связь между программой и внешним ресурсом (текстовым файлом)
+
+            while (!sr.EndOfStream) // свойство, работает только на чтение, возвращает булевое значение (достигли ли конца файла?)
+            {
+                string word = sr.ReadLine();
+                arr[counter] = word;
+                counter++; // при выходе из цикла counter будет содержать актуальное кол-во эл-тов
+            }
+
+            string[] buf = new string[counter]; // создание нового массива соразмерного массиву считанному из файла
+
+            Array.Copy(arr, buf, counter); // метод позволяет скопировать эл-ты одного массива в эл-ты другого массива (откуда, куда, кол-во элементов)
+
+            sr.Close(); // закрываем класс StreamReader через метод Close
+            return buf;
+        }
+
+        /// <summary>
+        /// Метод вывода массива
+        /// </summary>
+        public void PrintArray()
+        {
+            if (arr == null) // проверка наличия файла с метода LoadArrayFromFile
+            {
+                Console.WriteLine("\nТекстовый файл не найден на диске! Проверьте пусть/имя файла!");
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write($"{arr[i]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        ////Метод проверки логина и пароля 
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool Access(string login, string password)
+        {
+
+            if (login == arr[0] & password == arr[1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    internal class Task4
+    {
+        static void Main(string[] args)
+        {
+            Account array01 = new Account(AppDomain.CurrentDomain.BaseDirectory + "Access.txt"); // обяъвление и инициализация массива array01 (считываем логин и пароль из файла в массив)
+
+            Console.WriteLine("ВХОД В СИСТЕМУ\nВведите логин и пароль для авторизации!\n====================================");
+
+            int i = 0;
+
+            do
+            {
+                Console.Write("\nЛогин: ");
+                string login = Console.ReadLine();  // вводим логин
+
+                Console.Write("Пароль: ");
+                string password = Console.ReadLine();  // вводим пароль
+
+                i++;  // запускаем отсчет попыток входа
+
+                if (array01.Access(login, password))
+                {
+                    Console.WriteLine("Добро пожаловать в личный кабинет!\n"); // вывод приветствия при возвращении true метода Access
+                    break; // выход из цикла
+                }
+                else
+                {
+                    if (i == 3)
+                    {
+                        Console.WriteLine("Неверный логин и/или пароль! Вы использовали 3 попытки авторизации, попробуйте авторизоваться через время!\n"); // вывод сообщения о трёх неудачных попытках входа
+                        break; // выход из цикла
+                    }
+
+                    Console.WriteLine("Неверный логин и/или пароль! Попробуйте ещё раз пожалуйста!\nУ вас осталось попыток: " + (3-i)); // вывод сообщения о неудачной попытке входа при возвращении false метода Access
+                }
+
+            } while (i < 3);
+
+            Console.ReadLine();
+
+        }
+    }
+}
